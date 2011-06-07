b08556e8c2a1d97334e60187e08089079db95486
Kania Adam <keniu21@gmail.com>
2011-06-07 02:26:37 -0700

    Zmienilem cos tam.

--- src/statki/ComputerAi.java
+++ src/statki/ComputerAi.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package gameships;
 
 import java.util.Random;
@@ -77,7 +72,7 @@ public class ShipsComputerAi {
   
 
 
-void SaveHit(int x,int y,int statek)  /// Pozniej dodam.
+void SaveHit(int x,int y,int statek)  /// Pozniej dodam. 
 {
 
 }
